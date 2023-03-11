3703944412deabb10094d4ba54fb71b38fbe025e
Andrii Rybalko <ary@jooble.com>
2023-03-12 01:23:34 +0200

    move source files to src folder

---
+++ src/content_script.js
@@ -0,0 +1,52 @@
+﻿// helper functions available for all page scripts
+(async () => {
+  // injects script to page
+  const s = document.createElement('script');
+  s.src = chrome.runtime.getURL('src/injected.js');
+  s.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
+
+// create Sval interpreter.
+// so code could run in content script context
+// (could get chrome object)
+  const interpreter = new Sval({
+    // ECMA Version of the code (5 | 6 | 7 | 8 | 9 | 10 | 2015 | 2016 | 2017 | 2018 | 2019)
+    ecmaVer: 2019,
+    // Whether the code runs in a sandbox
+    sandBox: true,
+  });
+
+  const commonFunctions = await (await fetch(chrome.runtime.getURL('dist/common.js'))).text();
+// wraps code to isolated func.
+// so page script couldn`t save state
+// bettween pushState and replaceState events
+  const wrapCode = (code) => `(()=>{${commonFunctions}\n${code}})();`;
+
+// runs scripts that matches specified url,
+// inside DOM context or content script context
+  const doWorkOnUrl = (url) => {
+    chrome.storage.sync.get('container', (items) => {
+
+      const page_scripts = items.page_scripts ?? items.container;
+      for (let key_url in page_scripts) {
+        if (url.match(new RegExp(key_url))) {
+          const val = page_scripts[key_url];
+          const code = wrapCode(val.code ?? val);
+
+          interpreter.run(code);
+        }
+      }
+    });
+  }
+
+  const doWorkOnUrlOnCurrentUrl = () => doWorkOnUrl(location.href);
+
+  doWorkOnUrlOnCurrentUrl();
+
+  document.addEventListener('popstate', doWorkOnUrlOnCurrentUrl);
+  document.addEventListener('pushState', doWorkOnUrlOnCurrentUrl);
+  document.addEventListener('replaceState', doWorkOnUrlOnCurrentUrl);
+  window.addEventListener('hashchange', doWorkOnUrlOnCurrentUrl);
+})();
---
+++ src/popup/popup.js
@@ -0,0 +1,81 @@
+﻿function save() {
+  var tab_url = document.getElementById("url_text").value;
+  var js_text = window.editor.getValue();
+
+  chrome.storage.sync.get("container", function (items) {
+    var container = items.container ?? {};
+
+    container[tab_url] = js_text;
+    chrome.storage.sync.set({container: container});
+  });
+}
+
+function remove() {
+  var tab_url = document.getElementById("url_text").value;
+  chrome.storage.sync.get("container", function (items) {
+    var container = items.container;
+
+    container[tab_url] = undefined;
+    chrome.storage.sync.set({container: container});
+  });
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\\/]/g, '\\$&'); // $& means the whole matched string
+}
+
+async function getCurrentTabUrl() {
+  let queryOptions = {active: true, currentWindow: true};
+  let tabs = await chrome.tabs.query(queryOptions);
+  return tabs[0].url;
+}
+
+const defaultText = `callWhen({
+  predicate: () => $('body').length > 0,
+  action: () => { console.log("Hello world!") },
+  endless: false,
+});`
+
+document.addEventListener('DOMContentLoaded', function () {
+  require.config({paths: {vs: '../../node_modules/monaco-editor/min/vs'}});
+  require(['vs/editor/editor.main'], async function () {
+    for (const fileName of ['jquery.d.ts', 'common.d.ts']) {
+      const fileUri = `./dist/${fileName}`;
+      const fileText = await (await fetch(chrome.runtime.getURL(fileUri))).text();
+      monaco.languages.typescript.javascriptDefaults.addExtraLib(fileText, fileUri);
+      monaco.editor.createModel(fileText, "typescript", monaco.Uri.parse(fileUri));
+    }
+
+    document.getElementById("save").onclick = save;
+    document.getElementById("remove").onclick = remove;
+    let tab_url = await getCurrentTabUrl();
+
+    chrome.storage.sync.get("container", function (items) {
+      let container = items.container;
+      let pageData = null;
+      for (var key_url in container) {
+        if (tab_url.match(new RegExp(key_url)))
+          pageData = {urlRegex: key_url, text: container[key_url]}
+      }
+      pageData = pageData ?? {
+        urlRegex: `^${escapeRegExp(tab_url)}$`,
+        text: defaultText,
+      };
+
+      document.getElementById("url_text").value = pageData.urlRegex;
+      window.editor = monaco.editor.create(document.getElementById('container'), {
+        value: pageData.text,
+        language: 'javascript',
+      });
+    });
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.ctrlKey && e.key === 's') {
+    // Prevent the Save dialog to open
+    e.preventDefault();
+
+    save();
+  }
+});
